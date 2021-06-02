84ebbe10889094d5c279534708b12633f0ecd8bc
ChapaevAnton <virsus2009@gmail.com>
2021-06-02 13:11:00 +0700

    add layout material Bottom navigation main menu
    *no create style

--- app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/MainActivity.kt
+++ app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/MainActivity.kt
@@ -12,34 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonMenuHome = findViewById<Button>(R.id.main_menu_home)
-        val buttonMenuFavorites = findViewById<Button>(R.id.main_menu_favorites)
-        val buttonMenuWatchLater = findViewById<Button>(R.id.main_menu_watch_later)
-        val buttonMenuMySelections = findViewById<Button>(R.id.main_menu_home_my_selections)
-        val buttonMenuSettings = findViewById<Button>(R.id.main_menu_settings)
 
-
-        buttonMenuHome.setOnClickListener {
-            showSnackBar(it, R.string.main_menu_home)
-        }
-
-        buttonMenuFavorites.setOnClickListener {
-            showSnackBar(it, R.string.main_menu_favorites)
-        }
-
-        buttonMenuWatchLater.setOnClickListener {
-            showSnackBar(it, R.string.main_menu_watch_later)
-        }
-
-        buttonMenuMySelections.setOnClickListener {
-            showSnackBar(it, R.string.main_menu_my_selections)
-        }
-
-        buttonMenuSettings.setOnClickListener {
-            showSnackBar(it, R.string.main_menu_settings)
-        }
 
     }
+
 
     private fun showSnackBar(view: View, text: Int) {
         Snackbar.make(view, text, Snackbar.LENGTH_LONG).apply {
