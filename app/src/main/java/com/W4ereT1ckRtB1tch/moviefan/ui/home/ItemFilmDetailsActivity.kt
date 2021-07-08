e072cf6a62174632be1e7537339811974f4a0a21
ChapaevAnton <virsus2009@gmail.com>
2021-07-08 10:59:18 +0700

    added panel styles
    *changed font sizes

--- app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/ui/home/ItemFilmDetailsActivity.kt
+++ app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/ui/home/ItemFilmDetailsActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.Toolbar
 import com.W4ereT1ckRtB1tch.moviefan.MainActivity
 import com.W4ereT1ckRtB1tch.moviefan.R
 import com.W4ereT1ckRtB1tch.moviefan.data.Film
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class ItemFilmDetailsActivity : AppCompatActivity() {
 
@@ -16,7 +16,7 @@ class ItemFilmDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_film_details)
 
-        val detailsTitle: Toolbar = findViewById(R.id.details_title_film)
+        val detailsTitle: CollapsingToolbarLayout = findViewById(R.id.details_title_film)
         val detailsPoster: AppCompatImageView = findViewById(R.id.details_poster_film)
         val detailsDescription: TextView = findViewById(R.id.details_description_film)
 
