91467f939988193195e14982ef54496398abf593
Chapaev Anton <virsus2009@gmail.com>
2021-08-08 14:42:48 +0700

    add custom view
    * added an element to the layout
    * initialization of parameters
    * size calculation
    * drawing the ring and background
    * conversion rating to degrees

--- app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/ui/custom/view/RatingRoundView.kt
+++ app/src/main/java/com/W4ereT1ckRtB1tch/moviefan/ui/custom/view/RatingRoundView.kt
@@ -1,13 +1,11 @@
 package com.W4ereT1ckRtB1tch.moviefan.ui.custom.view
 
 import android.content.Context
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.W4ereT1ckRtB1tch.moviefan.R
+import kotlin.math.min
 
 class RatingRoundView @JvmOverloads constructor(
     context: Context,
@@ -36,6 +34,14 @@ class RatingRoundView @JvmOverloads constructor(
     init {
         val attr =
             context.theme.obtainStyledAttributes(attributeSet, R.styleable.RatingRoundView, 0, 0)
+
+        try {
+            stroke = attr.getFloat(R.styleable.RatingRoundView_stroke_round, stroke)
+            progress = attr.getInt(R.styleable.RatingRoundView_progress, progress)
+        } finally {
+            attr.recycle()
+        }
+
 
         initPaint()
     }
@@ -71,10 +77,63 @@ class RatingRoundView @JvmOverloads constructor(
 
     }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = min(width, height).div(2f)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+
+        val widthMode = MeasureSpec.getMode(widthMeasureSpec)
+        val widthSize = MeasureSpec.getSize(widthMeasureSpec)
+
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        val heightSize = MeasureSpec.getSize(heightMeasureSpec)
+
+        val chooseWidth = getDimension(widthMode, widthSize)
+        val chooseHeight = getDimension(heightMode, heightSize)
+
+        val minSide = min(chooseWidth, chooseHeight)
+
+        centerX = minSide.div(2f)
+        centerY = minSide.div(2f)
+
+        setMeasuredDimension(minSide, minSide)
+
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+
+        drawRound(canvas) //кольцо прогресса
+    }
+
+    private fun drawRound(canvas: Canvas?) {
+        val scale = radius * 0.8f
+        oval.set(0f - scale, 0f - scale, scale, scale)
+
+        canvas?.let {
+            it.save()
+            it.translate(centerX, centerY)
+            it.drawCircle(0f, 0f, radius, backgroundPaint)
+            it.drawArc(oval, -90f, convertProgressToDegrees(progress), false, roundPaint)
+            it.restore()
+        }
+    }
+
     private fun getColorPaint(progress: Int) = when (progress) {
         in 0..25 -> Color.parseColor("#e84258")
         in 26..50 -> Color.parseColor("#fd8060")
         in 51..75 -> Color.parseColor("#fee191")
         else -> Color.parseColor("#b0d8a4")
+    }
+
+    private fun getDimension(mode: Int, size: Int): Int {
+        return when (mode) {
+            MeasureSpec.AT_MOST, MeasureSpec.EXACTLY -> size
+            else -> 300
+        }
+    }
+
+    private fun convertProgressToDegrees(progress: Int): Float {
+        return progress * 3.6f
     }
 }
